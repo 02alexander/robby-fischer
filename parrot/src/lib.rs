e856e7b17109f457a82fb5dd74131f189b5085e3
Alexander Berntsson <alexande.berntsson@gmail.com>
2023-07-15 23:02:23 +0200

    can follow game using websockets.

--- parrot/src/lib.rs
+++ parrot/src/lib.rs
@@ -3,9 +3,13 @@ use std::future::Future;
 use std::time::Duration;
 
 use anyhow::Result;
-use futures_util::StreamExt;
+use futures_util::{SinkExt, StreamExt};
 use serde::Deserialize;
 use serde_json::Deserializer;
+use std::sync::mpsc::{channel, Receiver};
+use tokio::select;
+use tokio::time::interval;
+use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -16,6 +20,11 @@ struct GameMeta {
 #[derive(Deserialize, Debug)]
 struct GameEvent {
     fen: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct StalkEvent {
+    d: Option<GameEvent>,
 }
 
 /// Gets the IDs of the current Lichess TV games.
@@ -33,10 +42,77 @@ pub fn tv_games() -> Result<HashMap<String, String>> {
     })
 }
 
+pub fn stalk_game(id: impl AsRef<str>) -> Result<Receiver<Result<String>>> {
+    let sri = "9uFl575FsPGH";
+    let url = format!(
+        "wss://socket5.lichess.org/watch/{}/white/v6?sri={}&v=0:8765",
+        id.as_ref(),
+        sri
+    );
+    let (send_event, recv_event) = channel::<Result<String>>();
+
+    std::thread::spawn(move || {
+        in_runtime(async {
+            let (ws, _) = match connect_async(url).await {
+                Ok(stuff) => stuff,
+                Err(err) => {
+                    _ = send_event.send(Err(err.into()));
+                    return;
+                }
+            };
+            let (mut writer, mut reader) = ws.split();
+            let mut interval = interval(Duration::from_millis(3500));
+            loop {
+                // dbg!("loop");
+                select! {
+                    read = reader.next() => {
+                        // dbg!(&read);
+                        match read {
+                            Some(Ok(msg)) => {
+                                match msg {
+                                    Message::Text(s) => {
+                                        if s != "0" {
+                                            dbg!(&s);
+                                            match serde_json::from_str::<StalkEvent>(&s) {
+                                                Ok(stalk_event) => {
+                                                    // dbg!(&stalk_event);
+                                                    if let Some(game_event) = stalk_event.d {
+                                                        if send_event.send(Ok(game_event.fen)).is_err() {
+                                                            return;
+                                                        }
+                                                    }
+                                                },
+                                                Err(e) => {
+                                                    eprintln!("{:?}", e);
+                                                }
+                                            }
+                                        }
+                                    },
+                                    _ => {}
+                                }
+                            },
+                            Some(Err(e)) => {
+                                let _ = send_event.send(Err(e.into()));
+                            }
+                            _ => { break },
+                        }
+                    },
+                    _ = interval.tick() => {
+                        if let Err(err) = writer.send(Message::Text("null".into())).await {
+                            _ = send_event.send(Err(err.into()));
+                        }
+                    }
+                }
+            }
+        })
+    });
+    Ok(recv_event)
+}
+
 /// Watches a Lichess game, returning the FEN as it changes.
-pub fn watch_game(id: impl AsRef<str>, mut on_fen: impl FnMut(&str)) -> Result<()> {
+pub fn watch_game(id: impl AsRef<str>) -> Result<Receiver<Result<String>>> {
     let url = format!("https://lichess.org/api/stream/game/{}", id.as_ref());
-    let (send_event, recv_event) = std::sync::mpsc::channel::<Result<GameEvent>>();
+    let (send_event, recv_event) = channel::<Result<String>>();
 
     std::thread::spawn(move || {
         in_runtime(async {
@@ -54,11 +130,11 @@ pub fn watch_game(id: impl AsRef<str>, mut on_fen: impl FnMut(&str)) -> Result<(
                     Ok(bytes) => {
                         buffer.extend_from_slice(&bytes);
 
-                        let mut deser = Deserializer::from_slice(&buffer).into_iter();
+                        let mut deser = Deserializer::from_slice(&buffer).into_iter::<GameEvent>();
                         loop {
                             match deser.next() {
                                 Some(Ok(event)) => {
-                                    if send_event.send(Ok(event)).is_err() {
+                                    if send_event.send(Ok(event.fen)).is_err() {
                                         return;
                                     }
                                 }
@@ -88,16 +164,7 @@ pub fn watch_game(id: impl AsRef<str>, mut on_fen: impl FnMut(&str)) -> Result<(
 
     std::thread::sleep(Duration::from_millis(500));
 
-    while let Ok(result) = recv_event.recv() {
-        let mut event = result?;
-        while let Ok(result) = recv_event.try_recv() {
-            event = result?;
-        }
-
-        on_fen(&event.fen);
-    }
-
-    Ok(())
+    Ok(recv_event)
 }
 
 fn in_runtime<R>(f: impl Future<Output = R>) -> R {
--- planner/src/bin/play.rs
+++ planner/src/bin/play.rs
@@ -1,62 +1,64 @@
-use std::io::BufRead;
-
 use nalgebra::Vector3;
 use nix::sys::termios::BaudRate;
-use parrot::watch_game;
+use parrot::stalk_game;
 use planner::{
     arm::Arm,
-    board::{Board, Pieceholder},
-    chess::{Action, Position, Square},
+    board::Board,
+    chess::Position,
     termdev::TerminalDevice,
 };
 
 fn main() -> anyhow::Result<()> {
-    // let mut td = TerminalDevice::new("/dev/serial/by-id/usb-Raspberry_Pi_Pico_1234-if00")?;
-    // td.configure(BaudRate::B115200)?;
-    // td.set_timeout(1)?;
-    // let mut arm = Arm::new(td);
+    let mut td = TerminalDevice::new("/dev/serial/by-id/usb-Raspberry_Pi_Pico_1234-if00")?;
+    td.configure(BaudRate::B115200)?;
+    td.set_timeout(1)?;
+    let mut arm = Arm::new(td);
 
-    // arm.check_calib();
+    arm.check_calib();
 
-    // arm.bottom_angle_offset = 49.84891891479492;
-    // arm.top_angle_offset = 38.3333625793457;
-    // arm.translation_offset = Vector3::new(-0.13726842403411865, -0.0736, 0.0026969648897647858);
+    arm.bottom_angle_offset = 49.84891891479492;
+    arm.top_angle_offset = 38.3333625793457;
+    arm.translation_offset = Vector3::new(-0.13726842403411865, -0.0736, 0.0026969648897647858);
 
-    // arm.release();
-    // arm.sync_pos()?;
-    // println!("{}", arm.claw_pos);
-    // if arm.claw_pos.z < 0.4 && arm.claw_pos.z > 0.0 && arm.claw_pos.x > 0.0 && arm.claw_pos.x < 0.8
-    // {
-    //     arm.smooth_move_z(0.12);
-    // } else {
-    //     arm.move_claw_to(Vector3::new(0.10, 0.0, 0.29));
-    // }
+    arm.release();
+    arm.sync_pos()?;
+    println!("{}", arm.claw_pos);
+    if arm.claw_pos.z < 0.4 && arm.claw_pos.z > 0.0 && arm.claw_pos.x > 0.0 && arm.claw_pos.x < 0.8
+    {
+        arm.smooth_move_z(0.12);
+    } else {
+        arm.move_claw_to(Vector3::new(0.10, 0.0, 0.29));
+    }
 
     let mut board = Board::default();
 
-    let id = parrot::tv_games()?["Bullet"].to_owned();
+    // let id = parrot::tv_games()?["Bullet"].to_owned();
+    let id = "uktvwtZI";
     dbg!(&id);
 
-    watch_game(id, |fen| {
-        println!("{fen}");
+    // let recv = watch_game(id)?;
+    let recv = stalk_game(id)?;
+    while let Ok(result) = recv.recv() {
+        let mut fen = result?;
+        while let Ok(result) = recv.try_recv() {
+            fen = result?;
+        }
 
-        // println!();
         let position = Position::from_partial_fen(fen.split_ascii_whitespace().next().unwrap());
         let actions = board.position.diff(position);
         for action in actions {
             println!("{action:?}");
+            match action {
+                planner::chess::Action::Move(src, dst) => {
+                    board.move_piece(&mut arm, src, dst);
+                }
+                planner::chess::Action::Add(sq, piece) => board.add_piece(&mut arm, sq, piece),
+                planner::chess::Action::Remove(sq, _piece) => board.remove_piece(&mut arm, sq),
+            }
         }
-        // for action in actions {
-        //     match action {
-        //         planner::chess::Action::Move(src, dst) => {
-        //             board.move_piece(&mut arm, src, dst);
-        //         }
-        //         planner::chess::Action::Add(sq, piece) => board.add_piece(&mut arm, sq, piece),
-        //         planner::chess::Action::Remove(sq, _piece) => board.remove_piece(&mut arm, sq),
-        //     }
-        // }
         board.position = position;
-    })?;
+
+    }
 
     Ok(())
 }
